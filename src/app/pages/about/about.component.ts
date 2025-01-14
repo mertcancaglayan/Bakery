4d79effcde0a56d66cc0e235c12ff2667e4d47d6
M.can <caglayan.mertcan37@gmail.com>
2025-01-14 15:28:24 +0300

    update pages

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { AboutUsComponent } from "../../components/about-us/about-us.component";
-import { OurHistoryComponent } from "../../components/our-history/our-history.component";
+import { AboutUsComponent } from '../../components/about-us/about-us.component';
+import { OurHistoryComponent } from '../../components/our-history/our-history.component';
+import { OurServicesComponent } from '../../components/our-services/our-services.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [AboutUsComponent, OurHistoryComponent],
+  imports: [AboutUsComponent, OurHistoryComponent, OurServicesComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
