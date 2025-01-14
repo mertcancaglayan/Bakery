4c937b0c35aa364ec65ade09d1d73c08e1395765
M.can <caglayan.mertcan37@gmail.com>
2025-01-14 17:46:43 +0300

    add our-location component

---
+++ src/app/components/our-locations/our-locations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-locations',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-locations.component.html',
+  styleUrl: './our-locations.component.scss'
+})
+export class OurLocationsComponent {
+
+}
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MapComponent } from "../../components/map/map.component";
 import { ContactInfoComponent } from "../../components/contact-info/contact-info.component";
 import { ContactFormComponent } from "../../components/contact-form/contact-form.component";
+import { OurLocationsComponent } from "../../components/our-locations/our-locations.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [MapComponent, ContactInfoComponent, ContactFormComponent],
+  imports: [MapComponent, ContactInfoComponent, ContactFormComponent, OurLocationsComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
