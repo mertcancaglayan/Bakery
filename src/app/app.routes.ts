3b60408cc8d1d73523010fa77eebed6378ea555e
M.can <caglayan.mertcan37@gmail.com>
2025-02-05 15:12:50 +0300

    update single product page image style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { SingleProductPageComponent } from './pages/single-product-page/single-product-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'product/:id', component: SingleProductPageComponent },
   { path: '**', redirectTo: 'home' },
 ];
